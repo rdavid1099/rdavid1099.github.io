bb1cea2e38f6271958cede36c4847010ca492c41
Ryan Workman <rdavid1099@users.noreply.github.com>
2021-04-15 09:02:26 -0700

    Add functionalit to power button to turn off screen

--- src/components/ScreenContainer/ScreenContainer.tsx
+++ src/components/ScreenContainer/ScreenContainer.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from "react";
 import { ScreenContainerStyles } from "./ScreenContainer.style";
+import { useAppState } from "../../hooks/useAppState";
 
 export const ScreenContainer: FC = ({ children }) => {
+  const { state } = useAppState();
+
   return (
-    <div style={ScreenContainerStyles.root}>
-      <div style={ScreenContainerStyles.viewport}>{children}</div>
+    <div style={state.screenOn ? ScreenContainerStyles.screenOn : ScreenContainerStyles.screenOff}>
+      <div style={ScreenContainerStyles.viewport}>{state.screenOn && children}</div>
     </div>
   );
 };
--- src/components/ScreenController/ScreenController.tsx
+++ src/components/ScreenController/ScreenController.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, MouseEvent } from "react";
 import { ScreenControllerStyles } from "./ScreenController.style";
 import { PowerIcon } from "../../icons";
 import { GITHUB_URL } from "../../constants/urls";
+import { useAppState } from "../../hooks/useAppState";
 
 const RyanLogo: FC = () => {
   const { name, r, y, a, n } = ScreenControllerStyles;
@@ -16,24 +17,46 @@ const RyanLogo: FC = () => {
 };
 
 export const ScreenController: FC = () => {
+  const { state: appState, updateState } = useAppState();
+  const {
+    root,
+    nameContainer,
+    rainbowLine,
+    rdavid,
+    powerButtonContainer,
+    powerButtonOn,
+    powerButtonOff,
+    powerIcon,
+    powerLightOn,
+    powerLightOff,
+  } = ScreenControllerStyles;
+
+  const handlePowerButtonClick = (event: MouseEvent) => {
+    event.preventDefault();
+
+    const { screenOn } = appState;
+
+    updateState({ screenOn: !screenOn });
+  };
+
   return (
-    <div style={ScreenControllerStyles.root}>
-      <div style={ScreenControllerStyles.nameContainer}>
+    <div style={root}>
+      <div style={nameContainer}>
         <RyanLogo />
-        <div style={ScreenControllerStyles.rainbowLine} />
-        <div style={ScreenControllerStyles.rdavid}>
+        <div style={rainbowLine} />
+        <div style={rdavid}>
           <a href={GITHUB_URL} target="_blank" rel="noreferrer noopener">
             @rdavid1099
           </a>
         </div>
       </div>
-      <div style={ScreenControllerStyles.powerButtonContainer}>
-        <div style={ScreenControllerStyles.powerButton}>
-          <div style={ScreenControllerStyles.powerIcon}>
+      <div style={powerButtonContainer}>
+        <a href="#" style={appState.screenOn ? powerButtonOn : powerButtonOff} onClick={handlePowerButtonClick}>
+          <div style={powerIcon}>
             <PowerIcon />
           </div>
-          <div style={ScreenControllerStyles.powerLight} />
-        </div>
+          <div style={appState.screenOn ? powerLightOn : powerLightOff} />
+        </a>
       </div>
     </div>
   );
